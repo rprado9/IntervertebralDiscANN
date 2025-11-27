9a2855af745c82434a50dcc7582a10c2a202d5ad
rafaelprado <rafaelprado0902@gmail.com>
2025-11-27 10:32:50 -0300

    Add files via upload

---
+++ geometry_disc/geometry_disc.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# This code aims to approach the mapping points of the disc geometry.
+# this is necessery to generate the patterned mesh.
+# -------------------------------------------------------------------
+# This function has as inputs 
+
+def disc_geometry():
+    
+    pass
+
+if __name__=="__main__":
+
+    disc_geometry()
+    
+
